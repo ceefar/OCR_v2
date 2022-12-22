2ba9fc851a6681e462161f4222e531a194ed4597
Ceefar <rob.farq@googlemail.com>
2022-12-22 14:02:14 +0000

    about to add new branches now for versions

--- window_capture_v1_01.py
+++ window_capture_v1_01.py
@@ -3,8 +3,9 @@ import cv2 as cv # for img manipulation, etc
 import numpy as np # for array conversion 
 import pyautogui # for interface automation
 import os # for file handling
-import PIL # for img manipulation, etc
+from PIL import ImageGrab # for grabbing images only at present
 from time import perf_counter # for performance timing
+
 
 
 # -- testing functionality --
@@ -13,16 +14,24 @@ from time import perf_counter # for performance timing
 # -- update working directory to the folder this file is in --
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+
+# -- initialise performance timer --
+processing_timer = perf_counter()
+
 # -- loop until quit --
 while True:
 
     # -- take a full-screen screenshot, convert to matrix (assuming thats what mat stands for anyways, aka a multi dimensional array) for processing using np.array -- 
-    screenshot = pyautogui.screenshot()
+    screenshot = ImageGrab.grab() # about 3-5 fps improvement on avg vs pyautogui (which uses python image library anyways)
     screenshot = np.array(screenshot)
 
     # -- convert our screenshot img to bgr from opencv --
-    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR) # 0.0035s
-    # screenshot = screenshot[:, :, ::-1].copy() # 0.0148s # i guess this is just reordering the array by flipping the blue to the front using ::-1 array slice
+    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR) 
+
+    # -- print current frame rate for debugging, use floating point format string to change decimal precision --
+    current_time = perf_counter()
+    print(f"FPS : {1 / (current_time - processing_timer):.2f}")
+    processing_timer = perf_counter()
 
     # -- show screenshot in same window --
     cv.imshow('Comp Vision', screenshot)
